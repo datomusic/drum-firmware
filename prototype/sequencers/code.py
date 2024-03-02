3ab63a46d8ee9d9d774db0a79c5b525713bf45a9
Sundström Valter <mail@valter.nu>
2024-03-02 13:26:04 +0100

    Move out MidiTempo

--- prototype/sequencers/code.py
+++ prototype/sequencers/code.py
@@ -1,11 +1,9 @@
 import usb_midi
 import adafruit_midi
 from adafruit_midi.note_on import NoteOn
-from adafruit_midi.timing_clock import TimingClock
-from adafruit_midi.midi_continue import Continue
 
 from drum import Drum
-from tempo import Tempo
+from tempo import MidiTempo
 from note_output import NoteOutput
 from adafruit_midi.note_off import NoteOff
 
@@ -29,30 +27,20 @@ def main():
     midi = adafruit_midi.MIDI(midi_in=midi_in, midi_out=midi_out)
     drum = Drum()
     setup_tracks(drum.tracks)
-    tempo = Tempo(12)
+    tempo = MidiTempo()
 
     note_out = NoteOutput(
         lambda note, vel: midi.send(NoteOn(note, vel)),
         lambda note: midi.send(NoteOff(note)),
     )
 
-    def tick_drum():
-        def play_note(note, vel):
-            note_out.play(note, vel)
-
-        drum.tick(play_note)
+    def on_tick():
+        drum.tick(note_out.play)
+        note_out.tick()
 
     while True:
         msg = midi.receive()
-
-        if msg is not None:
-            if type(msg) is TimingClock:
-                tempo.tick(tick_drum)
-                note_out.tick()
-
-            elif type(msg) is Continue:
-                tempo.reset()
-                tick_drum()
+        tempo.handle_message(msg, on_tick)
 
 
 main()
--- prototype/sequencers/tempo.py
+++ prototype/sequencers/tempo.py
@@ -1,3 +1,7 @@
+from adafruit_midi.timing_clock import TimingClock
+from adafruit_midi.midi_continue import Continue
+
+
 class Tempo:
     def __init__(self, ticks_per_beat):
         self.ticks_per_beat = ticks_per_beat
@@ -11,3 +15,16 @@ class Tempo:
 
     def reset(self):
         self.ticks = 0
+
+
+class MidiTempo:
+    def __init__(self):
+        self.temp = Tempo(12)
+
+    def handle_message(self, msg, on_tick):
+        if type(msg) is TimingClock:
+            self.tempo.tick(on_tick)
+
+        elif type(msg) is Continue:
+            self.tempo.reset()
+            on_tick()

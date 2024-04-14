2d5eeba2dd63f3f3d8ec8e00ec11b9d1064e4b6c
Sundström Valter <mail@valter.nu>
2024-04-14 23:10:08 +0200

    Move code.py for teensy41 into devices/teensy41

---
+++ prototype/devices/teensy41/code.py
@@ -0,0 +1,14 @@
+import usb_midi  # type: ignore
+from adafruit_midi import MIDI  # type: ignore
+from firmware.application import Application
+from firmware.midi_output import MIDIOutput
+from teensy41.pizza_controller import PizzaController
+
+(midi_in, midi_out) = usb_midi.ports
+midi = MIDI(midi_in=midi_in, midi_out=midi_out)
+MIDIOutput(midi)
+
+controllers = [PizzaController()]
+output = MIDIOutput(midi)
+
+Application(controllers, output).run()
---
+++ prototype/firmware/midi_output.py
@@ -0,0 +1,33 @@
+from .device_api import Output
+from adafruit_midi import MIDI  # type: ignore
+from adafruit_midi.control_change import ControlChange  # type: ignore
+from adafruit_midi.note_on import NoteOn  # type: ignore
+from adafruit_midi.note_off import NoteOff  # type: ignore
+
+
+class MIDIOutput(Output):
+    def __init__(self, midi: MIDI):
+        self.midi = midi
+
+    def set_filter(self, channel, value) -> None:
+        print("Filter:", value)
+        self._send_msg(ControlChange(74, value), channel)
+
+    def send_note_on(self, channel: int, note: int, vel_percent: float):
+        self._send_msg(NoteOn(note, percent_to_midi(vel_percent)), channel)
+
+    def send_note_off(self, channel: int, note: int):
+        self._send_msg(NoteOff(note), channel)
+
+    def set_channel_pitch(self, channel: int, pitch_percent: float):
+        print(f"pitch_percent: {pitch_percent}")
+        self._send_msg(ControlChange(
+            16, percent_to_midi(pitch_percent)), channel)
+
+    def _send_msg(self, msg, channel):
+        print(f"{msg}, channel: {channel}")
+        self.midi.send(msg, channel)
+
+
+def percent_to_midi(percent):
+    return int((percent * 127) / 100)

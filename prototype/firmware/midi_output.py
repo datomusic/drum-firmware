4447706d968aa3dd988b4bc714721b232c4b84bf
Sundström Valter <mail@valter.nu>
2024-04-22 01:32:49 +0200

    Separate filter into lowpass and highpass range

--- prototype/firmware/midi_output.py
+++ prototype/firmware/midi_output.py
@@ -1,34 +1,40 @@
-from .device_api import Output
+from .device_api import Output, OutputParam
 from adafruit_midi import MIDI  # type: ignore
 from adafruit_midi.control_change import ControlChange  # type: ignore
 from adafruit_midi.note_on import NoteOn  # type: ignore
 from adafruit_midi.note_off import NoteOff  # type: ignore
+import math
 
 
 class MIDIOutput(Output):
     def __init__(self, midi: MIDI):
         self.midi = midi
 
-    def set_filter(self, amount_percent) -> None:
-        self._print_send(ControlChange(74, percent_to_midi(amount_percent)))
-
     def send_note_on(self, channel: int, note: int, vel_percent: float):
-        self.midi.send(NoteOn(note, percent_to_midi(vel_percent)), channel=channel)
+        self.midi.send(
+            NoteOn(note, percent_to_midi(vel_percent)), channel=channel)
 
     def send_note_off(self, channel: int, note: int):
         self.midi.send(NoteOff(note), channel=channel)
 
     def set_channel_pitch(self, channel: int, pitch_percent: float):
-        self._print_send(
-            ControlChange(16 + channel, percent_to_midi(pitch_percent))
-        )
+        self.send_cc(16 + channel, percent_to_midi(pitch_percent))
 
-    def set_volume(self, vol_percent):
-        self._print_send(ControlChange(7, percent_to_midi(vol_percent)))
+    def set_param(self, param, percent) -> None:
+        if param == OutputParam.Volume:
+            self.send_cc(7, percent_to_midi(percent))
 
-    def _print_send(self, msg):
-        print(msg)
-        self.midi.send(msg)
+        elif param == OutputParam.HighPass:
+            self.send_cc(
+                74, 64 + math.floor(percent_to_midi(percent) / 2))
+
+        elif param == OutputParam.LowPass:
+            self.send_cc(
+                74, 64 - math.floor(percent_to_midi(percent) / 2))
+
+    def send_cc(self, cc, value, channel=1):
+        print(f"CC {cc}: {value}")
+        self.midi.send(ControlChange(cc, value), channel=channel)
 
 
 def percent_to_midi(percent):

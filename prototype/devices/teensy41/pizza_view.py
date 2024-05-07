7ae8f65c89a32dad24fb9b60bb8e9cc1e1cf798d
Sundström Valter <mail@valter.nu>
2024-05-07 20:38:51 +0200

    Move cursor to own type

--- prototype/devices/teensy41/pizza_view.py
+++ prototype/devices/teensy41/pizza_view.py
@@ -52,31 +52,41 @@ class SequencerRing:
         self.fade_remaining_ms = FADE_TIME_MS
 
     def show_steps(self, display: Display, drum: Drum, step_color) -> None:
-        cursor_step = drum.get_indicator_step(self.track_index)
         track = drum.tracks[self.track_index]
 
         for step_index, step in enumerate(track.sequencer.steps):
-            on_cursor = step_index == cursor_step
             color = None
-            if on_cursor:
-                color = ColorScheme.Cursor
-            else:
-                if self.fade_remaining_ms > 0:
-                    fade_amount = self.fade_remaining_ms / FADE_TIME_MS
-                    color = fade_color(step_color, fade_amount)
-                elif step.active:
-                    color = step_color
+            if self.fade_remaining_ms > 0:
+                fade_amount = self.fade_remaining_ms / FADE_TIME_MS
+                color = fade_color(step_color, fade_amount)
+            elif step.active:
+                color = step_color
 
-            display.set_color(SequencerKey(
-                step_index, self.track_index), color)
+            display.set_color(
+                SequencerKey(step_index, self.track_index),
+                color)
 
     def update(self, delta_ms: int) -> None:
         if self.fade_remaining_ms > 0:
             self.fade_remaining_ms -= delta_ms
 
 
+class Cursor:
+    def update(self, delta_ms: int):
+        pass
+
+    def show(self, display: Display,  drum: Drum):
+        for track_index in range(Track.Count):
+            step_index = drum.get_indicator_step(track_index)
+            display.set_color(
+                SequencerKey(step_index, track_index),
+                ColorScheme.Cursor
+            )
+
+
 class PizzaView():
     def __init__(self) -> None:
+        self.cursor = Cursor()
         self.pad_indicators = [
             PadIndicator(track_index)
             for track_index in range(Track.Count)
@@ -88,6 +98,8 @@ class PizzaView():
         ]
 
     def update(self, delta_ms: int) -> None:
+        self.cursor.update(delta_ms)
+
         for pad_indicator in self.pad_indicators:
             pad_indicator.update(delta_ms)
 
@@ -101,9 +113,10 @@ class PizzaView():
         for (track_index, track) in enumerate(drum.tracks):
             color = ColorScheme.Tracks[drum.tracks[track_index].note]
             self.rings[track_index].show_steps(display, drum, color)
-            self._show_pads(display, drum, color, track_index)
+            self._show_pad(display, drum, color, track_index)
+            self.cursor.show(display, drum)
 
-    def _show_pads(self, display, drum, color, track_index) -> None:
+    def _show_pad(self, display, drum, color, track_index) -> None:
         current_step_index = drum.get_indicator_step(track_index)
 
         track = drum.tracks[track_index]

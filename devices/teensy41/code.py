329026abcd877a7aa8d74d17e5bc24e939ba43b5
Sundström Valter <mail@valter.nu>
2024-08-20 19:20:21 +0200

    Use numpy.interp

--- devices/teensy41/code.py
+++ devices/teensy41/code.py
@@ -2,51 +2,67 @@ import board
 import audiocore
 import ulab.numpy as np
 import adafruit_wave
-import audiopwmio
 import audiomixer
 
-wave_file = adafruit_wave.open("samples/boink.wav")
-channels, bit_depth, sample_rate = (
-    wave_file.getnchannels(),
-    wave_file.getsampwidth() * 8,
-    wave_file.getframerate(),
-)
 
-assert channels == 1
-assert bit_depth == 8
-
-audio = audiopwmio.PWMAudioOut(board.D12)
-mixer = audiomixer.Mixer(
-    samples_signed=False,
-    bits_per_sample=8,
-    voice_count=4,
-    sample_rate=sample_rate,
-    channel_count=1,
-)
-
-audio.play(mixer)
-file_buffer = wave_file.readframes(1024)
-file_buffer_length = len(file_buffer)
-
-
-def play_downpitched_wav(speed_multiplier):
-    buffer_length = int(file_buffer_length / speed_multiplier)
-    play_buffer = np.zeros(buffer_length, dtype=np.uint8)
-
-    for i in range(buffer_length):
-        ind = int(i * speed_multiplier)
-        play_buffer[i] = file_buffer[ind]
-
-    audio_sample = audiocore.RawSample(
-        bytearray(play_buffer.tobytes()), sample_rate=sample_rate
+def run_pitchshift_demo(audio):
+    wave_file = adafruit_wave.open("samples/boink.wav")
+    channels, bit_depth, sample_rate = (
+        wave_file.getnchannels(),
+        wave_file.getsampwidth() * 8,
+        wave_file.getframerate(),
     )
 
-    voice = mixer.voice[0]
-    voice.play(audio_sample)
+    assert channels == 1
+    assert bit_depth == 8
+
+    mixer = audiomixer.Mixer(
+        samples_signed=False,
+        bits_per_sample=8,
+        voice_count=4,
+        sample_rate=sample_rate,
+        channel_count=1,
+    )
+
+    audio.play(mixer)
+
+    file_buffer = wave_file.readframes(1024)
+    file_buffer_length = len(file_buffer)
+    file_buffer_indices = np.arange(file_buffer_length)
+
+    def play_downpitched_wav(speed_multiplier):
+        out_sample_count = int(file_buffer_length / speed_multiplier)
+        out_indices = np.linspace(0, file_buffer_length, out_sample_count)
+
+        play_buffer = np.array(
+            np.interp(out_indices, file_buffer_indices, file_buffer),
+            dtype=np.uint8,
+        )
+
+        audio_sample = audiocore.RawSample(
+            bytearray(play_buffer.tobytes()), sample_rate=sample_rate
+        )
+
+        voice = mixer.voice[0]
+        voice.play(audio_sample)
+
+    import time
+
+    while True:
+        time.sleep(0.5)
+        play_downpitched_wav(0.6)
 
 
-import time
+WITH_PWM = True
 
-while True:
-    time.sleep(0.5)
-    play_downpitched_wav(0.6)
+if WITH_PWM:
+    import audiopwmio
+    audio = audiopwmio.PWMAudioOut(board.D12)
+    run_pitchshift_demo(audio)
+else:
+    import adafruit_wm8960
+    import audiobusio
+
+    i2s = audiobusio.I2SOut(board.AUDIO_TX_BCLK, board.AUDIO_TX_SYNC, board.AUDIO_TXD, main_clock=board.AUDIO_MCLK)
+    dac = adafruit_wm8960.WM8960(board.I2C())
+    run_pitchshift_demo(i2s)

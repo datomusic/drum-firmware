949c52ed31a342a3d9b4a75f68e7cf4a9bef6b29
Sundström Valter <mail@valter.nu>
2024-08-20 19:31:32 +0200

    Respond to MIDI

--- devices/teensy41/code.py
+++ devices/teensy41/code.py
@@ -3,6 +3,9 @@ import audiocore
 import ulab.numpy as np
 import adafruit_wave
 import audiomixer
+import usb_midi
+import adafruit_midi
+from adafruit_midi.note_on import NoteOn
 
 
 def run_pitchshift_demo(audio):
@@ -30,7 +33,7 @@ def run_pitchshift_demo(audio):
     file_buffer_length = len(file_buffer)
     file_buffer_indices = np.arange(file_buffer_length)
 
-    def play_downpitched_wav(speed_multiplier):
+    def play_at_speed(speed_multiplier):
         out_sample_count = int(file_buffer_length / speed_multiplier)
         out_indices = np.linspace(0, file_buffer_length, out_sample_count)
 
@@ -46,23 +49,33 @@ def run_pitchshift_demo(audio):
         voice = mixer.voice[0]
         voice.play(audio_sample)
 
-    import time
-
+    midi = adafruit_midi.MIDI(midi_in=usb_midi.ports[0], in_channel=0)
     while True:
-        time.sleep(0.5)
-        play_downpitched_wav(0.6)
+        msg = midi.receive()
+
+        if isinstance(msg, NoteOn) and msg.velocity != 0:
+            print(f"Note On: {msg.note}")
+            speed_multiplier = 2 * (msg.note / 127)
+            print(f"speed_multiplier: {speed_multiplier}")
+            play_at_speed(speed_multiplier)
 
 
-WITH_PWM = True
+WITH_PWM = False
 
 if WITH_PWM:
     import audiopwmio
+
     audio = audiopwmio.PWMAudioOut(board.D12)
     run_pitchshift_demo(audio)
 else:
     import adafruit_wm8960
     import audiobusio
 
-    i2s = audiobusio.I2SOut(board.AUDIO_TX_BCLK, board.AUDIO_TX_SYNC, board.AUDIO_TXD, main_clock=board.AUDIO_MCLK)
+    i2s = audiobusio.I2SOut(
+        board.AUDIO_TX_BCLK,
+        board.AUDIO_TX_SYNC,
+        board.AUDIO_TXD,
+        main_clock=board.AUDIO_MCLK,
+    )
     dac = adafruit_wm8960.WM8960(board.I2C())
     run_pitchshift_demo(i2s)

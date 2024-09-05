c468e0636f287a0c661d45328b10d5530dafea35
Sundström Valter <mail@valter.nu>
2024-09-05 18:17:07 +0200

    WIP: Make PizzaController use Drum instead of Controls

--- firmware/drum.py
+++ firmware/drum.py
@@ -4,7 +4,17 @@ from .sequencer import Sequencer
 
 
 class Drum:
-    def __init__(self, sequencer: Sequencer, output: Output, tempo: Tempo):
+    def __init__(self, sequencer: Sequencer, output: Output, tempo: Tempo, on_sample_trigger):
         self.tempo = tempo
         self.sequencer = sequencer
         self.output = output
+        self.on_sample_trigger = on_sample_trigger
+
+    def play_track_sample(self, track_index: int, velocity: float):
+        self.sequencer.tracks[track_index].play(velocity)
+        self.on_sample_trigger(track_index)
+
+    def set_playing(self, playing):
+        self.sequencer.playing = playing
+        if playing:
+            self.tempo.reset()

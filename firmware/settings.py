7b7c1bf2eea49a5fc7a31d35dce54154a3c6d933
Sundström Valter <mail@valter.nu>
2024-09-05 16:53:37 +0200

    Cache settings so .toml file is not read on each access

--- firmware/settings.py
+++ firmware/settings.py
@@ -1,9 +1,18 @@
 import os
 
 
+# TODO: Persist settings which are set during runtime
 class Settings:
+    def __init__(self):
+        self.cache = {}
+
     def set(self, key: str, value):
+        self.cache[key] = value
         print(f"Key: {key}, value: {value}")
 
-    def get(self, item):
-        return os.getenv(item)
+    def get(self, key: str):
+        item = self.cache.get(key)
+        if item is None:
+            item = os.getenv(key)
+            self.cache[key] = item
+        return item

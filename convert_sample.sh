1db9cd1e87a64ed3158dde31c76fe1b9280f9bf5
Sundström Valter <mail@valter.nu>
2024-09-05 13:36:21 +0200

    16bit in convert_sample.sh

--- convert_sample.sh
+++ convert_sample.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -i "$1" -bitexact -acodec pcm_u8 -ac 1 -ar 16000 -map_metadata -1 -bitexact "$2"
+ffmpeg -i "$1" -bitexact -acodec pcm_s16le -ac 1 -map_metadata -1 -bitexact "$2"

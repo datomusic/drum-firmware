c6c08245386a315b3c2bf80408fce5bc5bfe318a
Sundström Valter <mail@valter.nu>
2024-09-06 13:38:58 +0200

    Remove unused set_lowpass_filter and set_highpass_filter from Output

--- firmware/output_api.py
+++ firmware/output_api.py
@@ -20,12 +20,6 @@ class Output:
     def send_note_off(self, channel: int, note: int):
         _not_implemented("Output.send_note_off", note)
 
-    def set_lowpass_filter(self, amount_percent):
-        _not_implemented("Output.set_lowpass_filter", amount_percent)
-
-    def set_highpass_filter(self, amount_percent):
-        _not_implemented("Output.set_highpass_filter", amount_percent)
-
     def set_channel_pitch(self, channel: int, pitch: float):
         _not_implemented("Output.set_channel_pitch", channel, pitch)
 

1373127776b511c34a7d9a70cbf860dfa5a998f0
Sundström Valter <mail@valter.nu>
2024-09-24 17:12:11 +0200

    Add logger

--- firmware/application.py
+++ firmware/application.py
@@ -9,8 +9,8 @@ from .midi_handler import MIDIHandler
 from adafruit_midi import MIDI
 import adafruit_logging as logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)  # type: ignore
+metrics.PRINT_REPORT = False
+metrics.WITH_MEMORY_METRICS = False
 
 metrics.PRINT_REPORT = False
 metrics.WITH_MEMORY_METRICS = False
--- firmware/midi_handler.py
+++ firmware/midi_handler.py
@@ -1,20 +1,28 @@
+import adafruit_logging as logging  # type: ignore
 from .drum import Drum
 from .tempo import TempoSource
 from .settings import Settings
 from .sysex_handler import SysexHandler
-from adafruit_midi import MIDI, MIDIMessage  # type: ignore
+from adafruit_midi import MIDI  # type: ignore
 from adafruit_midi.timing_clock import TimingClock  # type: ignore
 from adafruit_midi.midi_continue import Continue  # type: ignore
 from adafruit_midi.start import Start  # type: ignore
 from adafruit_midi.stop import Stop  # type: ignore
 from adafruit_midi.system_exclusive import SystemExclusive  # type: ignore
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# TODO: Use real manufacturer ID
+__MANUFACTURER_ID = bytes([0x7D])
+
 
 class MIDIHandler:
     def __init__(self, midi: MIDI, settings: Settings) -> None:
         self.midi = midi
         self.settings = settings
-        self.sysex_handler = SysexHandler(self._send_message, settings)
+        self.sysex_handler = SysexHandler(self._send_sysex_data, settings)
+        logger.debug("MIDIHandler initialized")
 
     def update(self, drum: Drum, delta_ms: int) -> None:
         message = self.midi.receive()
@@ -27,9 +35,12 @@ class MIDIHandler:
             elif isinstance(message, Stop):
                 drum.sequencer.set_playing(False)
             elif isinstance(message, SystemExclusive):
-                self.sysex_handler.handle_sysex_data(message)
+                if message.manufacturer_id != __MANUFACTURER_ID:
+                    logger.error("Invalid manufacturer ID")
+                else:
+                    self.sysex_handler.handle_sysex_data(message)
 
             message = self.midi.receive()
 
-    def _send_message(self, message: MIDIMessage) -> None:
-        self.midi.send(message)
+    def _send_sysex_data(self, data: bytes) -> None:
+        self.midi.send(SystemExclusive(__MANUFACTURER_ID, data))

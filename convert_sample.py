8c9f6715e7a3e9f5c9f756ef9f43844979dd34d2
Sundström Valter <mail@valter.nu>
2024-10-17 15:46:34 +0200

    Remove convert_sample.sh

--- convert_sample.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# ffmpeg -i "$1" -bitexact -acodec pcm_u8 -ac 1 -ar 16000 -map_metadata -1 -bitexact "$2"
-# ffmpeg -i "$1" -bitexact -acodec pcm_s16le -ac 1 -ar 16000 -map_metadata -1 -bitexact "$2"
-# ffmpeg -i "$1" -bitexact -acodec pcm_s16le -ac 1 -ar 44100 -map_metadata -1 -bitexact "$2"
-ffmpeg -i "$1" -bitexact -acodec pcm_s16le -ac 1 -ar 22050 -map_metadata -1 -bitexact "$2"

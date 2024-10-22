e20d4f85d7bd04b4e18325868ad423cffa1b76dd
Sundström Valter <mail@valter.nu>
2024-10-22 12:07:40 +0200

    Pitching down also works

--- src/apps/sample_player/pitch_shifter.h
+++ src/apps/sample_player/pitch_shifter.h
@@ -4,6 +4,11 @@
 #include <AudioStream.h>
 #include <stdint.h>
 
+namespace PitchShifterSupport {
+int16_t quad_interpolate(int16_t d1, int16_t d2, int16_t d3, int16_t d4,
+                         double x);
+}
+
 template <typename Reader> struct PitchShifter {
   PitchShifter() : speed(1.5) {
   }
@@ -19,49 +24,7 @@ template <typename Reader> struct PitchShifter {
   }
 
   // Reader interface
-  void read_samples(int16_t *out, const uint16_t out_sample_count) {
-    // TODO: Stream in chunks instead of using a preallocated buffer.
-    // Requires returning how many samples were read from reader.read_samples().
-    static const uint32_t buffer_size = AUDIO_BLOCK_SAMPLES * 2;
-    int16_t buffer[buffer_size];
-
-    if (this->speed > 1) {
-      uint32_t read_count = out_sample_count * this->speed;
-      if (read_count > buffer_size) {
-        read_count = buffer_size;
-      }
-
-      reader.read_samples(buffer, read_count);
-      for (int target_index = 0; target_index < out_sample_count;
-           ++target_index) {
-        const uint32_t source_index = (int)(target_index * this->speed);
-        if (source_index < buffer_size) {
-
-          *out++ = buffer[source_index];
-        } else {
-          *out++ = 0;
-        }
-      }
-
-    } else {
-      // TODO: Pitch down
-      reader.read_samples(out, out_sample_count);
-      /*
-      int16_t buffer[8];
-      int16_t interpolationData[4] = {0, 0, 0, 0};
-
-      int counter = 0;
-      while (counter < sample_count) {
-        reader.read_samples(buffer, 8);
-        *out++ = buffer[0];
-        *out++ = buffer[2];
-        *out++ = buffer[4];
-        *out++ = buffer[6];
-        counter += 8;
-      }
-      */
-    }
-  }
+  void read_samples(int16_t *out, const uint16_t out_sample_count);
 
   void set_speed(const float speed) {
     if (speed < 0.1f) {
@@ -79,9 +42,34 @@ private:
   float speed;
 };
 
-namespace PitchShifterSupport {
-int16_t quad_interpolate(int16_t d1, int16_t d2, int16_t d3, int16_t d4,
-                         double x);
+template <typename Reader>
+void PitchShifter<Reader>::read_samples(int16_t *out,
+                                        const uint16_t out_sample_count) {
+  // TODO: Stream in chunks instead of using a preallocated buffer.
+  // Requires returning how many samples were read from reader.read_samples().
+  static const uint32_t buffer_size = AUDIO_BLOCK_SAMPLES * 2;
+  int16_t buffer[buffer_size];
+
+  if (this->speed < 1.01f && this->speed > 0.99f) {
+    reader.read_samples(out, out_sample_count);
+  } else {
+    uint32_t read_count = out_sample_count * this->speed;
+    if (read_count > buffer_size) {
+      read_count = buffer_size;
+    }
+
+    reader.read_samples(buffer, read_count);
+    for (int target_index = 0; target_index < out_sample_count;
+         ++target_index) {
+      const uint32_t source_index = (int)(target_index * this->speed);
+      if (source_index < buffer_size) {
+
+        *out++ = buffer[source_index];
+      } else {
+        *out++ = 0;
+      }
+    }
+  }
 }
 
 #endif /* end of include guard: PITCH_SHIFTER_H_0GR8ZAHC */

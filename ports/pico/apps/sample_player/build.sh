b911672446efc2da891f46a2e6d13370d42ab39c
Sundström Valter <mail@valter.nu>
2024-12-07 00:08:23 +0100

    Remove PICO_COPY_TO_RAM

--- ports/pico/apps/sample_player/build.sh
+++ ports/pico/apps/sample_player/build.sh
@@ -1,2 +1,2 @@
 # cmake -S . -B build && pushd build && make -j 16
-cmake -S . -B build -DPICO_COPY_TO_RAM=1 -DPICO_BOARD=pico2 && pushd build && make -j 16
+cmake -S . -B build -DPICO_BOARD=pico2 && pushd build && make -j 16

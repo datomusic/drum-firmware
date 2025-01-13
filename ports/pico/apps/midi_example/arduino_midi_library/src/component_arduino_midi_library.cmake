1e71f347d0d936fa8920a05846385a6f6b2cb5c8
Valter Sundström <valter.sundstrom@gmail.com>
2025-01-13 02:12:27 +0100

    Add arduino_midi_library

---
+++ ports/pico/apps/midi_example/arduino_midi_library/src/component_arduino_midi_library.cmake
@@ -0,0 +1,18 @@
+include_guard(GLOBAL)
+message("arduino_midi_library component is included.")
+
+target_sources(${EXECUTABLE_NAME} PRIVATE
+${CMAKE_CURRENT_LIST_DIR}/midi_Namespace.h
+${CMAKE_CURRENT_LIST_DIR}/midi_Defs.h
+${CMAKE_CURRENT_LIST_DIR}/midi_Message.h
+${CMAKE_CURRENT_LIST_DIR}/midi_Platform.h
+${CMAKE_CURRENT_LIST_DIR}/midi_Settings.h
+${CMAKE_CURRENT_LIST_DIR}/MIDI.cpp
+${CMAKE_CURRENT_LIST_DIR}/MIDI.hpp
+${CMAKE_CURRENT_LIST_DIR}/MIDI.h
+${CMAKE_CURRENT_LIST_DIR}/serialMIDI.h
+)
+
+target_include_directories(${EXECUTABLE_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)

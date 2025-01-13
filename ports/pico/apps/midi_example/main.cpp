4d538636138b39d7f5086fbc48c64e1a78369ac5
Valter Sundström <valter.sundstrom@gmail.com>
2025-01-13 02:09:46 +0100

    WIP

--- ports/pico/apps/midi_example/main.cpp
+++ ports/pico/apps/midi_example/main.cpp
@@ -1,14 +1,169 @@
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+#include "bsp/board.h"
+#include "lib/usb/usb.h"
+#include "midi_wrapper.h"
+#include "tusb.h"
 #include <math.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
+
+void handleNoteOn(byte channel, byte note, byte velocity) {
+}
+
+void handleNoteOff(byte channel, byte note, byte velocity) {
+}
+
+void led_blinking_task(void);
 
 int main() {
-  stdio_init_all();
+  // stdio_init_all();
+  board_init();
+  DatoUSB::init();
 
-  while (true) {
-    int c = getchar_timeout_us(0);
+  MIDI::init(
+      MIDI::Callbacks{.note_on = handleNoteOn, .note_off = handleNoteOff});
+
+  static uint32_t last_ms = board_millis();
+  while (1) {
+    DatoUSB::background_update();
+    led_blinking_task();
+    const uint32_t now_ms = board_millis();
+    if (now_ms - last_ms > 1000) {
+      last_ms = now_ms;
+      MIDI::sendNoteOn(70, 127, 1);
+    }
+    /*
+    sleep_ms(200);
+    MIDI::sendNoteOff(70, 127, 1);
+    sleep_ms(1000);
+    MIDI::sendNoteOn(58, 127, 1);
+    sleep_ms(200);
+    MIDI::sendNoteOff(58, 127, 1);
+    sleep_ms(1000);
+    */
   }
 
   puts("\n");
   return 0;
+}
+
+enum {
+  BLINK_NOT_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500,
+};
+
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
+void midi_task(void);
+
+/*
+int main() {
+  board_init();
+
+  while (1)
+  {
+    tud_task(); // tinyusb device task
+    led_blinking_task();
+    midi_task();
+  }
+}
+*/
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void) {
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void) {
+  blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en) {
+  (void)remote_wakeup_en;
+  blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void) {
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+//--------------------------------------------------------------------+
+// MIDI Task
+//--------------------------------------------------------------------+
+
+// Variable that holds the current position in the sequence.
+uint32_t note_pos = 0;
+
+// Store example melody as an array of note values
+uint8_t note_sequence[] = {
+    74, 78, 81, 86,  90, 93, 98, 102, 57, 61,  66, 69, 73, 78, 81, 85,
+    88, 92, 97, 100, 97, 92, 88, 85,  81, 78,  74, 69, 66, 62, 57, 62,
+    66, 69, 74, 78,  81, 86, 90, 93,  97, 102, 97, 93, 90, 85, 81, 78,
+    73, 68, 64, 61,  56, 61, 64, 68,  74, 78,  81, 86, 90, 93, 98, 102};
+
+void midi_task(void) {
+  static uint32_t start_ms = 0;
+  uint8_t msg[3];
+
+  // send note every 1000 ms
+  if (board_millis() - start_ms < 286)
+    return; // not enough time
+  start_ms += 286;
+
+  // Previous positions in the note sequence.
+  int previous = note_pos - 1;
+
+  // If we currently are at position 0, set the
+  // previous position to the last note in the sequence.
+  if (previous < 0)
+    previous = sizeof(note_sequence) - 1;
+
+  // Send Note On for current position at full velocity (127) on channel 1.
+  msg[0] = 0x90;                    // Note On - Channel 1
+  msg[1] = note_sequence[note_pos]; // Note Number
+  msg[2] = 127;                     // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Send Note Off for previous note.
+  msg[0] = 0x80;                    // Note Off - Channel 1
+  msg[1] = note_sequence[previous]; // Note Number
+  msg[2] = 0;                       // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Increment position
+  note_pos++;
+
+  // If we are at the end of the sequence, start over.
+  if (note_pos >= sizeof(note_sequence))
+    note_pos = 0;
+}
+
+//--------------------------------------------------------------------+
+// BLINKING TASK
+//--------------------------------------------------------------------+
+void led_blinking_task(void) {
+  static uint32_t start_ms = 0;
+  static bool led_state = false;
+
+  // Blink every interval ms
+  if (board_millis() - start_ms < blink_interval_ms)
+    return; // not enough time
+  start_ms += blink_interval_ms;
+
+  board_led_write(led_state);
+  led_state = 1 - led_state; // toggle
 }

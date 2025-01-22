f0377a9dc6a3f37f95c7a761a7f5724d57b2cb3a
Valter Sundström <valter.sundstrom@gmail.com>
2025-01-22 23:26:47 +0100

    Use baud_rate argument

--- ports/pico/examples/midi_example/pico_uart.cpp
+++ ports/pico/examples/midi_example/pico_uart.cpp
@@ -3,14 +3,17 @@
 #include "hardware/uart.h"
 
 // TODO: Make UART configurable. Currently defaulting to UART 0.
+// TODO: Also make pins configurable
 #define UART UART_INSTANCE(0)
+#define PIN_TX 1
+#define PIN_RX 1
+
 
 void PicoUART::begin(unsigned long baud_rate) {
-  uart_init(UART, 31250);
-
+  uart_init(UART, baud_rate);
   // Set the GPIO pin mux to the UART - 0 is TX, 1 is RX
-  gpio_set_function(0, GPIO_FUNC_UART);
-  gpio_set_function(1, GPIO_FUNC_UART);
+  gpio_set_function(PIN_TX, GPIO_FUNC_UART);
+  gpio_set_function(PIN_RX, GPIO_FUNC_UART);
 }
 
 uint8_t PicoUART::read() {

ba53131a1d39cf81ee0de8a9b39afd881f06d880
Valter Sundström <valter.sundstrom@gmail.com>
2025-03-26 00:31:56 +0100

    WIP: musin_init.cmake with usb and filesystem support

--- musin/musin_init.cmake
+++ musin/musin_init.cmake
@@ -1,20 +1,42 @@
 set(CMAKE_C_STANDARD 11)
 set(CMAKE_CXX_STANDARD 17)
 
-
 set(MUSIN_ROOT ${CMAKE_CURRENT_LIST_DIR})
 
+set(SDK_PATH ${MUSIN_ROOT}/ports/pico/pico-sdk/)
+set(SDK_EXTRAS_PATH ${MUSIN_ROOT}/ports/pico/pico-extras/)
+
+# initialize pico-sdk from submodule
+# note: this must happen before project()
+include(${SDK_PATH}/pico_sdk_init.cmake)
+include(${SDK_EXTRAS_PATH}/external/pico_extras_import.cmake)
+
 macro(musin_init TARGET)
+
+  set(MUSIN_SHARED ${MUSIN_ROOT}/shared)
+  set(LIB_DIR ${MUSIN_ROOT}/ports/pico/libraries)
+  set(MUSIN_AUDIO ${MUSIN_SHARED}/audio)
+  set(MUSIN_USB ${MUSIN_SHARED}/usb)
+
+  add_subdirectory(${LIB_DIR}/pico-vfs build)
+
   target_include_directories(${TARGET} PRIVATE
-    ${MUSIN_ROOT}/shared/
+    ${MUSIN_SHARED}
+    ${MUSIN_USB}
+    ${LIB_DIR}/arduino_midi_library/src
+    ${LIB_DIR}/Arduino-USBMIDI/src
   )
 
   target_sources(${TARGET} PRIVATE
-    ${MUSIN_ROOT}/shared/audio/audio_output.cpp
-    ${MUSIN_ROOT}/shared/audio/pitch_shifter.cpp
-    ${MUSIN_ROOT}/shared/audio/audio_memory_reader.cpp
-    ${MUSIN_ROOT}/shared/audio/data_ulaw.c
-    ${MUSIN_ROOT}/shared/audio/mixer.cpp
+    ${MUSIN_AUDIO}/audio_output.cpp
+    ${MUSIN_AUDIO}/pitch_shifter.cpp
+    ${MUSIN_AUDIO}/audio_memory_reader.cpp
+    ${MUSIN_AUDIO}/data_ulaw.c
+    ${MUSIN_AUDIO}/mixer.cpp
+    # ${MUSIN_USB}/usb.cpp
+    # ${MUSIN_USB}/usb_descriptors.c
+    # ${MUSIN_USB}/midi_usb_bridge/MIDIUSB.cpp
+    # ${MUSIN_SHARED}/filesystem/filesystem.c
   )
 
   target_compile_definitions(${TARGET} PRIVATE
@@ -22,13 +44,19 @@ macro(musin_init TARGET)
     USE_AUDIO_I2S=1
   )
 
-  target_link_libraries(${TARGET}
+  target_link_libraries(${TARGET} PRIVATE
     pico_stdlib
     hardware_dma
     hardware_pio
     hardware_irq
     pico_audio_i2s
+    tinyusb_device
+    tinyusb_board
   )
+
+  pico_sdk_init()
+  pico_add_extra_outputs(${TARGET})
+
+  # pico_enable_filesystem(${EXECUTABLE_NAME})
+
 endmacro()
-
-
--- musin/shared/usb/midi_usb_bridge/MIDIUSB.cpp
+++ musin/shared/usb/midi_usb_bridge/MIDIUSB.cpp
@@ -7,7 +7,7 @@ midiEventPacket_t MidiUSB_::read(void) {
   midiEventPacket_t result{0, 0, 0, 0};
   uint8_t recv_packet[4];
 
-  if (Musin::midi_read(recv_packet)) {
+  if (Musin::Usb::midi_read(recv_packet)) {
     result.header = recv_packet[0];
     result.byte1 = recv_packet[1];
     result.byte2 = recv_packet[2];
@@ -22,5 +22,5 @@ void MidiUSB_::flush(void) {}
 void MidiUSB_::sendMIDI(const midiEventPacket_t event) {
   const uint8_t packet[4] = {event.header, event.byte1, event.byte2,
                              event.byte3};
-  Musin::midi_send(packet);
+  Musin::Usb::midi_send(packet);
 }

2508e6640e2c889c916dc6ea95de0605169fe4a1
Valter Sundström <valter.sundstrom@gmail.com>
2025-03-26 21:52:05 +0100

    Separate out usb-midi to not clash with stdio

--- musin/musin_init.cmake
+++ musin/musin_init.cmake
@@ -2,6 +2,8 @@ set(CMAKE_C_STANDARD 11)
 set(CMAKE_CXX_STANDARD 17)
 
 set(MUSIN_ROOT ${CMAKE_CURRENT_LIST_DIR})
+set(MUSIN_LIBRARIES ${MUSIN_ROOT}/ports/pico/libraries)
+set(MUSIN_USB ${MUSIN_ROOT}/usb)
 
 set(SDK_PATH ${MUSIN_ROOT}/ports/pico/pico-sdk/)
 set(SDK_EXTRAS_PATH ${MUSIN_ROOT}/ports/pico/pico-extras/)
@@ -12,13 +14,28 @@ include(${SDK_PATH}/pico_sdk_init.cmake)
 include(${SDK_EXTRAS_PATH}/external/pico_extras_import.cmake)
 
 macro(musin_init TARGET)
-
-  set(MUSIN_LIBRARIES ${MUSIN_ROOT}/ports/pico/libraries)
-  set(MUSIN_AUDIO ${MUSIN_ROOT}/audio)
-  set(MUSIN_USB ${MUSIN_ROOT}/usb)
-
   target_include_directories(${TARGET} PRIVATE
     ${MUSIN_ROOT}/..
+  )
+
+  target_sources(${TARGET} PRIVATE
+    ${MUSIN_ROOT}/pico_uart.cpp
+  )
+
+  target_link_libraries(${TARGET} PRIVATE
+    pico_stdlib
+  )
+
+  pico_sdk_init()
+  pico_enable_stdio_usb(${TARGET} 1)
+  pico_enable_stdio_uart(${TARGET} 1)
+  pico_add_extra_outputs(${TARGET})
+
+endmacro()
+
+macro(musin_init_usb_midi TARGET)
+
+  target_include_directories(${TARGET} PRIVATE
     ${MUSIN_USB}
     ${MUSIN_USB}/midi_usb_bridge
     ${MUSIN_LIBRARIES}/arduino_midi_library/src
@@ -30,30 +47,22 @@ macro(musin_init TARGET)
     ${MUSIN_USB}/usb_descriptors.c
     ${MUSIN_USB}/midi_usb_bridge/MIDIUSB.cpp
     ${MUSIN_ROOT}/midi/midi_wrapper.cpp
-    ${MUSIN_ROOT}/pico_uart.cpp
   )
 
   target_link_libraries(${TARGET} PRIVATE
-    pico_stdlib
-    hardware_dma
-    hardware_pio
-    hardware_irq
     tinyusb_device
     tinyusb_board
   )
 
-  pico_sdk_init()
-
   # Using USB-MIDI will currently break stdio-over-usb, until we've defined a CDC device in the usb descriptors.
   # Currently we then get this warning from tinyusb: "stdio USB was configured along with user use of TinyUSB device mode, but CDC is not enabled"
   # Reference for defining CDC device: https://github.com/raspberrypi/pico-sdk/tree/master/src/rp2_common/pico_stdio_usb
-  pico_enable_stdio_usb(${TARGET} 1)
-  pico_enable_stdio_uart(${TARGET} 1)
-  pico_add_extra_outputs(${TARGET})
-
+  pico_enable_stdio_usb(${TARGET} 0)
 endmacro()
 
 macro(musin_init_audio TARGET)
+  set(MUSIN_AUDIO ${MUSIN_ROOT}/audio)
+
   target_sources(${TARGET} PRIVATE
     ${MUSIN_AUDIO}/audio_output.cpp
     ${MUSIN_AUDIO}/pitch_shifter.cpp
@@ -68,6 +77,9 @@ macro(musin_init_audio TARGET)
   )
 
   target_link_libraries(${TARGET} PRIVATE
+    hardware_dma
+    hardware_pio
+    hardware_irq
     pico_audio_i2s
   )
 endmacro()

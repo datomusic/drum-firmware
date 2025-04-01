a903e228dbcac0f5a162851c0edf487ec8a7080d
David Menting <david@davidmenting.nl>
2025-04-01 21:47:03 +0200

    Get rid of commented C++ class

--- musin/audio/aic3204.h
+++ musin/audio/aic3204.h
@@ -8,13 +8,6 @@
 #define AIC3204_SOFT_STEPPING_TIMEOUT_MS 1000
 #define AIC3204_AMP_ENABLE_THROUGH_CODEC true
 
-// class Aic3204 {
-// public:
-//   static bool init(unsigned int sda_pin, unsigned int scl_pin, unsigned int baudrate);
-//   static bool write_register(unsigned int page, unsigned int reg_addr, unsigned int value);
-//   static bool read_register(unsigned int page, unsigned int reg_addr, unsigned int* read_value);
-// };
-
 #ifdef __cplusplus
 extern "C" {
 #endif

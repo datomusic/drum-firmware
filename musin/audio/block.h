3c645efd471fd0970c1b0ea6dab419850c53e75a
Valter Sundström <valter.sundstrom@gmail.com>
2025-04-05 23:53:14 +0200

    Add missing block.h

---
+++ musin/audio/block.h
@@ -0,0 +1,10 @@
+
+#ifndef BLOCK_H_WDR6BFX7
+#define BLOCK_H_WDR6BFX7
+
+#include "etl/vector.h"
+#include <stdint.h>
+
+typedef etl::vector<int16_t, AUDIO_BLOCK_SAMPLES> AudioBlock;
+
+#endif /* end of include guard: BLOCK_H_WDR6BFX7 */

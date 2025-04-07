2fedc2eaf9840a055b53059c5207d3a564177006
David Menting <david@davidmenting.nl>
2025-04-07 13:12:26 +0200

    fix: Adjust filter frequency and resonance control in sample player

--- experiments/sample_player/main.cpp
+++ experiments/sample_player/main.cpp
@@ -94,10 +94,10 @@ void handle_cc(byte channel, byte controller, byte value) {
   break;
 
   case 75: // Filter Frequency
-    lowpass.frequency(normalized_value); // Use the Lowpass wrapper method
+    lowpass.filter.frequency(normalized_value * 10000.0f);
     break;
   case 76: // Filter Resonance
-    lowpass.resonance(normalized_value); // Use the Lowpass wrapper method
+    lowpass.filter.resonance(normalized_value);
     break;
 
   case 77: // Crusher Bits (Squish)

0250fc56502cd751e21b87e6d7ecfa26f2ba072c
David Menting <david@davidmenting.nl>
2025-04-10 15:56:23 +0200

    refactor: simplify CMake configuration and update board definitions

--- musin/boards/dato_submarine.h
+++ musin/boards/dato_submarine.h
@@ -2,7 +2,7 @@
  * Custom board definition for DATO Submarine
  */
 
-// pico_cmake_set PICO_PLATFORM=rp2350
+// pico_cmake_set PICO_PLATFORM=rp2350-arm-s
 
 #ifndef _BOARDS_DATO_SUBMARINE_H
 #define _BOARDS_DATO_SUBMARINE_H
@@ -50,8 +50,16 @@
 #ifndef PICO_FLASH_SPI_CLKDIV
 #define PICO_FLASH_SPI_CLKDIV 2
 #endif
+
+// pico_cmake_set_default PICO_FLASH_SIZE_BYTES = (16 * 1024 * 1024)
 #ifndef PICO_FLASH_SIZE_BYTES
 #define PICO_FLASH_SIZE_BYTES (16 * 1024 * 1024)
+#endif
+
+
+// pico_cmake_set_default PICO_RP2350_A2_SUPPORTED = 1
+#ifndef PICO_RP2350_A2_SUPPORTED
+#define PICO_RP2350_A2_SUPPORTED 1
 #endif
 
 #endif

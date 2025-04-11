e067c52f816eaa955921790e2c3070b7cbc630d8
David Menting <david@davidmenting.nl>
2025-04-11 16:52:35 +0200

    refactor: Rename send_midi to _send_midi for clarity

--- experiments/midi_cc/observers.h
+++ experiments/midi_cc/observers.h
@@ -17,19 +17,18 @@ struct MIDICCObserver : public Musin::UI::AnalogControlObserverBase {
     const uint8_t midi_channel;
     
     // Function pointer type defined elsewhere (e.g., main.cpp)
-    using MIDISendFn = void (*)(uint8_t channel, uint8_t cc, uint8_t value);
-    const MIDISendFn send_midi;
+    const MIDISendFn _send_midi;
     
     // Constructor
     constexpr MIDICCObserver(uint8_t cc, uint8_t channel, MIDISendFn sender)
-        : cc_number(cc), midi_channel(channel), send_midi(sender) {}
+        : cc_number(cc), midi_channel(channel), _send_midi(sender) {}
     
     void on_value_changed([[maybe_unused]] uint16_t control_id, float new_value, [[maybe_unused]] uint16_t raw_value) override {
         // Convert normalized value (0.0-1.0) to MIDI CC value (0-127)
         uint8_t cc_value = static_cast<uint8_t>(new_value * 127.0f);
         
         // Send MIDI CC message through function pointer
-        send_midi(midi_channel, cc_number, cc_value);
+        _send_midi(midi_channel, cc_number, cc_value);
     }
 };
 

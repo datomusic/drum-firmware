19725fb69db1c46707bb41ec3840e96ca44adb3e
Valter Sundström <valter.sundstrom@gmail.com>
2025-04-13 00:02:41 +0200

    Make drum/main.cpp compile again

--- drum/main.cpp
+++ drum/main.cpp
@@ -1,45 +1,16 @@
-#include "musin/audio/mixer.h"
+#include "musin/audio/audio_output.h"
 #include "musin/filesystem/filesystem.h"
 #include "musin/midi/midi_wrapper.h"
 #include "musin/usb/usb.h"
-#include "tusb.h"
-
-// TODO: Pico stdio stuff should live in shared code
+#include <stdio.h>
 #include <pico/stdlib.h>
 
-#include <stdio.h>
-
 #define SHOULD_REFORMAT false
-
-static const uint master_volume = 10;
-
-static void __not_in_flash_func(fill_audio_buffer)(audio_buffer_t *out_buffer) {
-
-  static int16_t temp_samples[AUDIO_BLOCK_SAMPLES];
-  // mixer.fill_buffer(temp_samples);
-
-  int16_t *stereo_out_samples = (int16_t *)out_buffer->buffer->bytes;
-  for (int i = 0; i < AUDIO_BLOCK_SAMPLES; ++i) {
-    stereo_out_samples[i] = (master_volume * temp_samples[i]) >> 8u;
-  }
-
-  out_buffer->sample_count = AUDIO_BLOCK_SAMPLES;
-}
 
 static bool init() {
   Musin::Usb::init();
   stdio_init_all();
-
-  MIDI::init(MIDI::Callbacks{
-      .note_on = nullptr,
-      .note_off = nullptr,
-      .clock = nullptr,
-      .start = nullptr,
-      .cont = nullptr,
-      .stop = nullptr,
-      .cc = nullptr,
-      .sysex = nullptr,
-  });
+  sleep_ms(1000);
 
   // stdio over USB doesn't always work directly after statup.
   // Adding sleeps doesn't seem to help, but printing some dots does.
@@ -48,15 +19,27 @@ static bool init() {
   }
 
   printf("Startup\n");
-
   printf("Initializing filesystem\n");
 
   if (!Musin::Filesystem::init(SHOULD_REFORMAT)) {
     printf("Filesystem initialization failed\n");
     return false;
   }
-
   printf("Filesystem initialized\n");
+
+
+  MIDI::init(MIDI::Callbacks {
+      .note_on = nullptr,
+      .note_off = nullptr,
+      .clock = nullptr,
+      .start = nullptr,
+      .cont = nullptr,
+      .stop = nullptr,
+      .cc = nullptr,
+      .pitch_bend = nullptr,
+      .sysex = nullptr,
+  });
+
 
   return true;
 }
@@ -73,7 +56,7 @@ int main(void) {
   printf("Entering main loop!\n");
 
   while (true) {
-    AudioOutput::update(fill_audio_buffer);
+    // AudioOutput::update(fill_audio_buffer);
     Musin::Usb::background_update();
     MIDI::read(1);
   }

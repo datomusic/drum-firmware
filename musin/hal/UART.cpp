5cea3aead2d414e05d4445e0f66aa6c126eccb06
David Menting <david@davidmenting.nl>
2025-04-13 19:51:35 +0200

    Remove no longer needed UART.cpp

--- musin/hal/UART.cpp
+++
@@ -1,5 +0,0 @@
-// Implementation is now in the header file (UART.h) because it's a template class.
-// This file can be left empty or removed from the build if it's no longer needed
-// for other definitions. For now, keep it empty.
-
-// #include "musin/hal/UART.h" // No longer needed here if empty

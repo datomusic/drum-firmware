1ba3239d358f8d191711f100ad1f28de01862cc3
David Menting <david@davidmenting.nl>
2025-04-17 16:14:00 +0200

    feat: Add pizza_controls.h and pizza_display.h

---
+++ experiments/pizza_example/pizza_controls.h
@@ -0,0 +1,16 @@
+/*
+
+Holds the four sliders
+Holds the four drumpads
+Holds the analog controls
+Holds the keypad
+
+*/
+
+handle_drumpad_event(pad, event);
+
+handle_effectpad_event(pad, event); // velocity on effect pads, maybe unused
+
+handle_control_event(pad, event); // sliders, effect pads, playbutton, swing switch, tempo pot, volume pot
+
+handle_keypad_event(); // sequencer buttons, sample select buttons

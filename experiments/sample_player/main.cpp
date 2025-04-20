df5f3103f9e456cf2d45bcb8047d3eaaa05a1c46
Valter Sundström <valter.sundstrom@gmail.com>
2025-04-20 21:50:31 +0200

    WIP: samples in sample_player experiment

--- experiments/sample_player/main.cpp
+++ experiments/sample_player/main.cpp
@@ -8,38 +8,143 @@
 #include "musin/audio/mixer.h"
 #include "musin/audio/sound.h"
 
-#include "samples/AudioSampleCashregister.h"
-#include "samples/AudioSampleGong.h"
-#include "samples/AudioSampleHihat.h"
-#include "samples/AudioSampleKick.h"
-#include "samples/AudioSampleSnare.h"
+#include "samples/005_data.c"
+#include "samples/006_data.c"
 
-#include "musin/audio/audio_memory_reader.h"
-#include "musin/audio/file_reader.h"
-
-using Musin::Audio::FileReader;
-
-struct MemorySound {
-  MemorySound(const unsigned int *sample_data, const uint32_t data_length)
-      : reader(sample_data, data_length), sound(Sound(reader)) {
+// Reads Mono 16bit PCM samples from memory
+struct PcmReader : SampleReader {
+  constexpr PcmReader(const unsigned char *bytes, const uint32_t byte_count) {
+    set_source(bytes, byte_count);
   }
 
-  AudioMemoryReader reader;
+  constexpr void set_source(const unsigned char *bytes, const uint32_t byte_count) {
+    this->bytes = bytes;
+    this->end = this->bytes + byte_count;
+  }
+
+  // Reader interface
+  constexpr void reset() {
+    iterator = bytes;
+  }
+
+  // Reader interface
+  constexpr bool has_data() {
+    return iterator != nullptr;
+  }
+
+  // Reader interface
+  constexpr uint32_t read_samples(AudioBlock &out_samples) {
+    auto out = out_samples.begin();
+    unsigned char tmp1;
+    unsigned char tmp2;
+
+    unsigned samples_written = 0;
+    for (samples_written = 0; samples_written < AUDIO_BLOCK_SAMPLES; samples_written++) {
+      if (!read_next(tmp1)) {
+        break;
+      }
+
+      if (!read_next(tmp2)) {
+        break;
+      }
+
+      const uint16_t upper = tmp1 >> 8;
+      *out++ = upper + tmp2;
+    }
+
+    if (samples_written == 0) {
+      iterator = nullptr;
+    }
+
+    return samples_written;
+  }
+
+private:
+  constexpr bool read_next(unsigned char &out) {
+    if (iterator == nullptr) {
+      return false;
+    }
+
+    if (iterator == end) {
+      iterator = nullptr;
+      return false;
+    } else {
+      out = *iterator;
+      ++iterator;
+      return true;
+    }
+  };
+
+  const unsigned char *bytes;
+  const unsigned char *end;
+  const unsigned char *iterator;
+};
+
+struct SampleData {
+  const unsigned char *data;
+  const uint32_t length;
+};
+
+constexpr static const etl::array<SampleData, 4> sample_bank = {
+    // static const etl::array<SampleData, 32> sample_bank = {
+    SampleData{samples_005__pcm, samples_005__pcm_len},
+    SampleData{samples_006__pcm, samples_006__pcm_len},
+    SampleData{samples_006__pcm, samples_006__pcm_len},
+    SampleData{samples_006__pcm, samples_006__pcm_len},
+    /*
+    SampleData{samples_015__pcm, samples_015__pcm_len},
+    SampleData{samples_100_nt_snare__pcm, samples_100_nt_snare__pcm_len},
+    SampleData{samples_26880__vexst__closed_hi_hat_2_1__pcm,
+               samples_26880__vexst__closed_hi_hat_2_1__pcm_len},
+    SampleData{samples_26887__vexst__kick_3_1__pcm, samples_26887__vexst__kick_3_1__pcm_len},
+    SampleData{samples_26901__vexst__snare_2_1__pcm, samples_26901__vexst__snare_2_1__pcm_len},
+    SampleData{samples_44_Analog_Cowbell__pcm, samples_44_Analog_Cowbell__pcm_len},
+    SampleData{samples_cabasa__pcm, samples_cabasa__pcm_len},
+    SampleData{samples_chihiro_snare__pcm, samples_chihiro_snare__pcm_len},
+    SampleData{samples_cowbell_hi__pcm, samples_cowbell_hi__pcm_len},
+    SampleData{samples_ClosedHH_909X_2__pcm, samples_ClosedHH_909X_2__pcm_len},
+    SampleData{samples_DR110_clap__pcm, samples_DR110_clap__pcm_len},
+    SampleData{samples_DR55HAT__pcm, samples_DR55HAT__pcm_len},
+    SampleData{samples_DR55RIM__pcm, samples_DR55RIM__pcm_len},
+    SampleData{samples_duo_hat_01__pcm, samples_duo_hat_01__pcm_len},
+    SampleData{samples_duo_kick_01__pcm, samples_duo_kick_01__pcm_len},
+    SampleData{samples_DUO_snare_01__pcm, samples_DUO_snare_01__pcm_len},
+    SampleData{samples_FR_BB_Sarik_HHat_010_1__pcm, samples_FR_BB_Sarik_HHat_010_1__pcm_len},
+    SampleData{samples_FR_BB_Sarik_Snare_004_1__pcm, samples_FR_BB_Sarik_Snare_004_1__pcm_len},
+    SampleData{samples_Finger_Snap__pcm, samples_Finger_Snap__pcm_len},
+    SampleData{samples_JR_SDD_HAT_A1_mono__pcm, samples_JR_SDD_HAT_A1_mono__pcm_len},
+    SampleData{samples_JR_SDD_KICK_1_1__pcm, samples_JR_SDD_KICK_1_1__pcm_len},
+    SampleData{samples_JR_SDD_SNARE_10__pcm, samples_JR_SDD_SNARE_10__pcm_len},
+    SampleData{samples_KEMP8_SET1_54_004__pcm, samples_KEMP8_SET1_54_004__pcm_len},
+    SampleData{samples_Kick_C78__pcm, samples_Kick_C78__pcm_len},
+    SampleData{samples_Kick_909_23__pcm, samples_Kick_909_23__pcm_len},
+    SampleData{samples_skclhat__pcm, samples_skclhat__pcm_len},
+    SampleData{samples_Snare_909_3__pcm, samples_Snare_909_3__pcm_len},
+    SampleData{samples_Snare_C78_with_silence__pcm, samples_Snare_C78_with_silence__pcm_len},
+    SampleData{samples_vocal_3__pcm, samples_vocal_3__pcm_len},
+    SampleData{samples_Zap_2__pcm, samples_Zap_2__pcm_len}
+    */
+};
+
+struct MemorySound {
+  constexpr MemorySound(const size_t sample_index)
+      : sample_index(sample_index),
+        reader(sample_bank[sample_index].data, sample_bank[sample_index].length),
+        sound(Sound(reader)) {
+  }
+
+  size_t sample_index;
+  PcmReader reader;
   Sound sound;
 };
 
-FileReader reader;
+MemorySound kick(0);
+MemorySound snare(1);
+MemorySound clap(2);
+MemorySound hihat(3);
 
-const uint8_t master_volume = 10;
+const etl::array<BufferSource *, 4> sounds = {&kick.sound, &snare.sound, &hihat.sound, &clap.sound};
 
-MemorySound kick(AudioSampleKick, AudioSampleKickSize);
-MemorySound snare(AudioSampleSnare, AudioSampleSnareSize);
-MemorySound gong(AudioSampleGong, AudioSampleGongSize);
-MemorySound cashreg(AudioSampleCashregister, AudioSampleCashregisterSize);
-MemorySound hihat(AudioSampleHihat, AudioSampleHihatSize);
-
-const etl::array<BufferSource *, 4> sounds = {&kick.sound, &snare.sound, &hihat.sound,
-                                              &cashreg.sound};
 AudioMixer mixer(sounds);
 
 Crusher crusher(mixer);
@@ -115,3 +220,19 @@ int main() {
 
   return 0;
 }
+
+consteval int test_memory_sound() {
+  // samples_005__pcm, samples_005__pcm_len
+  MemorySound sound1(0);
+  MemorySound sound2(0);
+  MemorySound sound3(1);
+  MemorySound sound4(1);
+
+  sound1.sound.play(1.0);
+  AudioBlock block;
+  sound1.sound.fill_buffer(block);
+
+  return 0;
+}
+
+static_assert(test_memory_sound() == 0);

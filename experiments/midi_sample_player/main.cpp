71171eaf6088044257d90e95b0942797105461d2
Valter Sundström <valter.sundstrom@gmail.com>
2025-04-20 18:59:06 +0200

    WIP

--- experiments/midi_sample_player/main.cpp
+++ experiments/midi_sample_player/main.cpp
@@ -4,13 +4,13 @@
 #include <cstdio>
 
 #include "musin/audio/audio_output.h"
-#include "musin/audio/block.h"
 #include "musin/audio/buffer_source.h"
 #include "musin/audio/crusher.h"
 #include "musin/audio/filter.h"
 #include "musin/audio/mixer.h"
 #include "musin/midi/midi_wrapper.h"
 #include "musin/usb/usb.h"
+#include "samples.h"
 
 #include "pico/time.h"
 
@@ -22,6 +22,50 @@
 #include "musin/audio/audio_memory_reader.h"
 #include "musin/audio/file_reader.h"
 #include "musin/audio/sound.h"
+
+struct SampleData {
+  unsigned char *data;
+  size_t length;
+};
+
+constexpr const size_t SampleCount = 1;
+SampleData sample_bank[SampleCount] = {
+    {samples_005__pcm, samples_005__pcm_len},
+    /*
+    {samples_006__pcm, samples_006__pcm_len},
+    {samples_015__pcm, samples_015__pcm_len},
+    {samples_100_nt_snare__pcm, samples_100_nt_snare__pcm_len},
+    {samples_26880__vexst__closed_hi_hat_2_1__pcm,
+     samples_26880__vexst__closed_hi_hat_2_1__pcm_len},
+    {samples_26887__vexst__kick_3_1__pcm, samples_26887__vexst__kick_3_1__pcm_len},
+    {samples_26901__vexst__snare_2_1__pcm, samples_26901__vexst__snare_2_1__pcm_len},
+    {samples_44_Analog_Cowbell__pcm, samples_44_Analog_Cowbell__pcm_len},
+    {samples_cabasa__pcm, samples_cabasa__pcm_len},
+    {samples_chihiro_snare__pcm, samples_chihiro_snare__pcm_len},
+    {samples_cowbell_hi__pcm, samples_cowbell_hi__pcm_len},
+    {samples_ClosedHH_909X_2__pcm, samples_ClosedHH_909X_2__pcm_len},
+    {samples_DR110_clap__pcm, samples_DR110_clap__pcm_len},
+    {samples_DR55HAT__pcm, samples_DR55HAT__pcm_len},
+    {samples_DR55RIM__pcm, samples_DR55RIM__pcm_len},
+    {samples_duo_hat_01__pcm, samples_duo_hat_01__pcm_len},
+    {samples_duo_kick_01__pcm, samples_duo_kick_01__pcm_len},
+    {samples_DUO_snare_01__pcm, samples_DUO_snare_01__pcm_len},
+    {samples_FR_BB_Sarik_HHat_010_1__pcm, samples_FR_BB_Sarik_HHat_010_1__pcm_len},
+    {samples_FR_BB_Sarik_Snare_004_1__pcm, samples_FR_BB_Sarik_Snare_004_1__pcm_len},
+    {samples_Finger_Snap__pcm, samples_Finger_Snap__pcm_len},
+    {samples_JR_SDD_HAT_A1_mono__pcm, samples_JR_SDD_HAT_A1_mono__pcm_len},
+    {samples_JR_SDD_KICK_1_1__pcm, samples_JR_SDD_KICK_1_1__pcm_len},
+    {samples_JR_SDD_SNARE_10__pcm, samples_JR_SDD_SNARE_10__pcm_len},
+    {samples_KEMP8_SET1_54_004__pcm, samples_KEMP8_SET1_54_004__pcm_len},
+    {samples_Kick_C78__pcm, samples_Kick_C78__pcm_len},
+    {samples_Kick_909_23__pcm, samples_Kick_909_23__pcm_len},
+    {samples_skclhat__pcm, samples_skclhat__pcm_len},
+    {samples_Snare_909_3__pcm, samples_Snare_909_3__pcm_len},
+    {samples_Snare_C78_with_silence__pcm, samples_Snare_C78_with_silence__pcm_len},
+    {samples_vocal_3__pcm, samples_vocal_3__pcm_len},
+    {samples_Zap_2__pcm, samples_Zap_2__pcm_len}};
+    */
+    };
 
 using Musin::Audio::FileReader;
 

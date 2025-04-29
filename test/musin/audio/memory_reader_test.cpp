a39075bd1beaebc06e54b511434332971e7a1705
Valter Sundström <valter.sundstrom@gmail.com>
2025-04-30 01:08:30 +0200

    Remove sample conversion stuff

--- test/musin/audio/memory_reader_test.cpp
+++ test/musin/audio/memory_reader_test.cpp
@@ -19,35 +19,8 @@
 #define REQUIRE assert
 #endif
 
-// #include <array>
-// #include <cstdint>
-
-/*
-// Converts an array of unsigned char to an array of int16_t at compile time.
-template <size_t N>
-consteval std::array<int16_t, N / 2> convertToInt16(const unsigned char input[N]) {
-  static_assert(N % 2 == 0, "Input size must be even to form 16-bit integers.");
-  std::array<int16_t, N / 2> result{};
-
-  for (size_t i = 0; i < N / 2; ++i) {
-    result[i] = static_cast<int16_t>((static_cast<int16_t>(input[2 * i]) << 8) | input[2 * i + 1]);
-  }
-
-  return result;
-}
-*/
-
 TEST_CASE("MemorySampleReader streams samples") {
   CONST_BODY(({
-    /*
-    // Example input array of unsigned char
-    constexpr unsigned char input[] = {0x01, 0x01, 0x03, 0x04, 0xFF, 0xFE, 0x00, 0x01};
-
-    // Convert at compile time
-    constexpr auto convertedArray = convertToInt16<8>(input);
-    REQUIRE(convertedArray[0] == 257);
-    */
-
     const int16_t samples[2] = {123, 33};
     MemorySampleReader decoder(samples, 2);
 

a31ce873fe1060e5372bfa9a9a10a9d632507a01
David Menting <david@davidmenting.nl>
2025-04-29 14:24:05 +0200

    style: Format main.cpp with clang-format

--- sb25_drum/main.cpp
+++ sb25_drum/main.cpp
@@ -38,36 +38,36 @@ int main() {
 
   pizza_display.init();
 
- pizza_controls.init();
+  pizza_controls.init();
 
- // --- Initialize Clocking System ---
- internal_clock.add_observer(tempo_handler);
- tempo_handler.add_observer(tempo_multiplier);
- tempo_multiplier.add_observer(sequencer_controller);
+  // --- Initialize Clocking System ---
+  internal_clock.add_observer(tempo_handler);
+  tempo_handler.add_observer(tempo_multiplier);
+  tempo_multiplier.add_observer(sequencer_controller);
 
- if (tempo_handler.get_clock_source() == Tempo::ClockSource::INTERNAL) {
-   internal_clock.start();
- }
- // TODO: Add logic to register TempoHandler with other clocks
- // TODO: Add logic to start/stop clocks based on TempoHandler source selection
+  if (tempo_handler.get_clock_source() == Tempo::ClockSource::INTERNAL) {
+    internal_clock.start();
+  }
+  // TODO: Add logic to register TempoHandler with other clocks
+  // TODO: Add logic to start/stop clocks based on TempoHandler source selection
 
- pizza_sequencer.get_track(0).get_step(0) = {36, 100, true};
- pizza_sequencer.get_track(0).get_step(4) = {36, 100, true};
- pizza_sequencer.get_track(1).get_step(2) = {38, 100, true};
- pizza_sequencer.get_track(1).get_step(6) = {38, 100, true};
+  pizza_sequencer.get_track(0).get_step(0) = {36, 100, true};
+  pizza_sequencer.get_track(0).get_step(4) = {36, 100, true};
+  pizza_sequencer.get_track(1).get_step(2) = {38, 100, true};
+  pizza_sequencer.get_track(1).get_step(6) = {38, 100, true};
 
- while (true) {
-   pizza_controls.update();
+  while (true) {
+    pizza_controls.update();
 
-   pizza_display.draw_sequencer_state(pizza_sequencer, sequencer_controller.get_current_step());
+    pizza_display.draw_sequencer_state(pizza_sequencer, sequencer_controller.get_current_step());
 
-   pizza_display.show();
-   Musin::Usb::background_update();
-   midi_read();
+    pizza_display.show();
+    Musin::Usb::background_update();
+    midi_read();
 
-   // Brief Delay: Important for WS2812 LED latching after show()
-   sleep_us(80);
- }
+    // Brief Delay: Important for WS2812 LED latching after show()
+    sleep_us(80);
+  }
 
   return 0;
 }

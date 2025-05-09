bae25ab20f0bf72b72b1a47d643b29e0e6901042
David Menting <david@davidmenting.nl>
2025-05-09 07:49:21 +0200

    refactor: Move model, view, controller declarations to sections

--- drum/main.cpp
+++ drum/main.cpp
@@ -1,35 +1,34 @@
 #include "musin/usb/usb.h"
+#include "musin/hal/debug_utils.h"
+#include "musin/timing/internal_clock.h"
+#include "musin/timing/step_sequencer.h"
+#include "musin/timing/sync_out.h"
+#include "musin/timing/tempo_handler.h"
 
 #include "pico/stdio_usb.h"
 #include "pico/time.h"
 
 #include "audio_engine.h"
 #include "midi_functions.h"
-#include "musin/hal/debug_utils.h"
-#include "musin/timing/internal_clock.h"
-#include "musin/timing/step_sequencer.h"
-#include "musin/timing/sync_out.h"
-#include "musin/timing/tempo_handler.h"
-// Removed: #include "musin/timing/tempo_multiplier.h"
 #include "pizza_controls.h"
 #include "pizza_display.h"
 #include "sequencer_controller.h"
 #include "sound_router.h"
 
-static drum::PizzaDisplay pizza_display;
-
+// Model
 static drum::AudioEngine audio_engine;
 static drum::SoundRouter sound_router(audio_engine);
-
-static musin::timing::Sequencer<4, 8> pizza_sequencer;
-static musin::timing::InternalClock internal_clock(120.0f);
+static musin::timing::Sequencer<4, 8> pizza_sequencer; // TODO: move into sequencer_controller
+static musin::timing::InternalClock internal_clock(120.0f); // TODO: move into sequencer_controller
 static musin::timing::TempoHandler tempo_handler(internal_clock,
                                                musin::timing::ClockSource::INTERNAL);
+drum::SequencerController sequencer_controller(pizza_sequencer, tempo_handler, sound_router);
 
-// static musin::timing::TempoMultiplier tempo_multiplier(24, 1); // Removed
+// View
+// TODO: notify pizza_display on note_on for drumpad fade
+static drum::PizzaDisplay pizza_display(pizza_sequencer, sequencer_controller, tempo_handler);
 
-drum::SequencerController sequencer_controller(pizza_sequencer, tempo_handler, sound_router); // Changed tempo_multiplier to tempo_handler
-
+// Controller
 static drum::PizzaControls pizza_controls(pizza_display, pizza_sequencer,
                                           tempo_handler, sequencer_controller,
                                           sound_router);
@@ -38,7 +37,6 @@ constexpr std::uint32_t SYNC_OUT_GPIO_PIN = 3;
 static musin::timing::SyncOut sync_out(SYNC_OUT_GPIO_PIN, internal_clock);
 
 // TODO: Instantiate MIDIClock, ExternalSyncClock when available
-// TODO: Add logic to dynamically change tempo_multiplier ratio if input PPQN changes
 
 static musin::hal::DebugUtils::LoopTimer loop_timer(1000);
 
@@ -60,8 +58,7 @@ int main() {
 
   // --- Initialize Clocking System ---
   internal_clock.add_observer(tempo_handler);
-  // tempo_handler.add_observer(tempo_multiplier); // Removed
-  tempo_handler.add_observer(sequencer_controller); // Added
+  tempo_handler.add_observer(sequencer_controller);
 
   sync_out.enable();
 

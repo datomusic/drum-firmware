5d3ec2f51a0339b9c1645925edd583ffe4f69a1a
Valter Sundström <valter.sundstrom@gmail.com>
2025-05-10 17:48:59 +0200

    WIP: Preparing for drum tests

---
+++ test/run_all_tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+./run_test_dir.sh musin
+./run_test_dir.sh drum
--- test/run_test_dir.sh
+++ test/run_test_dir.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+SOURCE_DIR="$1"
+
 set -e
 
+cd "$SOURCE_DIR"
+
 echo "--------------------------"
-echo "Executing tests"
+echo "Executing tests: $SOURCE_DIR"
 echo "--------------------------"
-cmake -DSTATIC_TESTS=0 -Bbuild -H. -DCMAKE_CXX_STANDARD=14 \
+cmake -Bbuild -H. -DCMAKE_CXX_STANDARD=14 \
   && pushd build && make -j 8 \
   && ctest --output-on-failure
 
 popd && rm -rf build
 
 echo "------------------------"
-echo "Building constexpr tests"
+echo "Building constexpr tests: $SOURCE_DIR"
 echo "------------------------"
 cmake -DSTATIC_TESTS=1 -Bbuild -H. -DCMAKE_CXX_STANDARD=14 \
   && pushd build \

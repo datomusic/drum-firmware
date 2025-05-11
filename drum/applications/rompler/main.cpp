84351873d7f49133a5b3f0e50799ad3cac75ebb4
Valter Sundström <valter.sundstrom@gmail.com>
2025-05-11 19:54:39 +0200

    WIP

--- drum/applications/rompler/main.cpp
+++ drum/applications/rompler/main.cpp
@@ -9,6 +9,22 @@
 #include "musin/audio/audio_output.h"
 #include "rompler.h"
 
+// File receiving:
+// - React to some file-transfer start event. SysEx message or something else.
+// - Open a source stream from the relevant transport (sysex, serial, whatever).
+// - Notify sink (saving to filesystem) about start of a new transfer. Essentially opens a file.
+// - Decode incoming data into bytes
+// - Keep two buffers
+// - Read into one buffer until full, or stream ends
+// - Switch buffers, and read following bytes into second one
+// - Pass the filled buffer to sink (which will write data to file)
+// - If 
+
+
+
+static void handle_sysex(byte *data, unsigned length) {
+}
+
 int main() {
   stdio_usb_init();
   musin::usb::init();
@@ -21,7 +37,7 @@ int main() {
   SampleBank bank;
   Rompler rompler(bank);
 
-  MIDI::init(MIDI::Callbacks{});
+  MIDI::init(MIDI::Callbacks{.sysex = handle_sysex});
 
   printf("Initializing audio output\n");
   if (!AudioOutput::init()) {

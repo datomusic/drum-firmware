3774b87431fa5b82332d4c8d34897aad0a6116eb
David Menting <david@davidmenting.nl>
2025-05-12 12:45:15 +0200

    fix: omit pipefail option for sh compatibility

--- experiments/support/generate_sample_headers.sh
+++ experiments/support/generate_sample_headers.sh
@@ -6,7 +6,7 @@
 # Ensure sox and xxd are installed
 
 set -e # Exit immediately if a command exits with a non-zero status.
-set -o pipefail # Ensure pipeline errors are caught
+# set -o pipefail # Ensure pipeline errors are caught
 
 # Define the directory containing the samples
 SAMPLE_DIR="samples"

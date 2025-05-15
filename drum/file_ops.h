20691e80fd6ea83a11b6a7b1b5e0e72aa84fabfc
Valter Sundström <valter.sundstrom@gmail.com>
2025-05-16 01:01:13 +0200

    Add file_ops.h

---
+++ drum/file_ops.h
@@ -0,0 +1,19 @@
+#ifndef FILE_OPS_H_BSDNSIZL
+#define FILE_OPS_H_BSDNSIZL
+
+#include "etl/delegate.h"
+#include "etl/span.h"
+#include "etl/string.h"
+
+template <typename Handle, int BlockSize> struct FileOps {
+  typedef Handle FileHandle;
+  typedef etl::string<64> Path;
+  typedef etl::delegate<Handle(const Path &path)> Open;
+
+  const Open open;
+  etl::delegate<void(Handle handle)> close;
+  etl::delegate<size_t(Handle handle, etl::array<uint8_t, BlockSize> &output)> read;
+  etl::delegate<void(Handle handle, const etl::span<const uint8_t, BlockSize> bytes)> write;
+};
+
+#endif /* end of include guard: FILE_OPS_H_BSDNSIZL */

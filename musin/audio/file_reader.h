810995aab462d367f71605cf88829cf8e33cf6bc
Valter Sundström <valter.sundstrom@gmail.com>
2025-05-17 18:53:36 +0200

    Don't miss the last smaples in FileReader

--- musin/audio/file_reader.h
+++ musin/audio/file_reader.h
@@ -26,17 +26,15 @@ struct FileReader : SampleReader {
   }
 
   void update() {
-    this->needs_update = false;
-    if (!data_available) {
+    if (!needs_update || !data_available) {
       return;
     }
 
+    this->needs_update = false;
+
     // TODO: Convert from source format to int16_t
-    read_count = fread(buffer, sizeof(int16_t), AUDIO_BLOCK_SAMPLES, handle);
+    read_count = fread(buffer, sizeof(int16_t), AudioBlock::MaxSamples, handle);
     // printf("read_count: %i\n", read_count);
-    if (read_count < AUDIO_BLOCK_SAMPLES) {
-      data_available = false;
-    }
   }
 
   void reset() {
@@ -55,11 +53,19 @@ struct FileReader : SampleReader {
   }
 
   uint32_t __not_in_flash_func(read_samples)(AudioBlock &out) {
+    if (!has_data()) {
+      return 0;
+    }
+
     // printf("[FileReader] read samples\n");
     this->needs_update = true;
     if (read_count > 0) {
       for (size_t i = 0; i < read_count; ++i) {
         out[i] = buffer[i];
+      }
+
+      if (read_count < out.size()) {
+        data_available = false;
       }
 
       return read_count;
@@ -68,14 +74,13 @@ struct FileReader : SampleReader {
     }
   }
 
-  bool needs_update;
-
 private:
   const char *file_name;
   size_t read_count = 0;
   FILE *handle;
   bool data_available;
-  int16_t buffer[AUDIO_BLOCK_SAMPLES];
+  int16_t buffer[AudioBlock::MaxSamples];
+  bool needs_update = false;
 };
 
 } // namespace musin::Audio

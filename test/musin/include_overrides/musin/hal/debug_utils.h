28d16a46785c4d68961e2f30a885215524844cd1
David Menting <david@davidmenting.nl>
2025-06-11 10:38:41 +0200

    style: format

--- test/musin/include_overrides/musin/hal/debug_utils.h
+++ test/musin/include_overrides/musin/hal/debug_utils.h
@@ -18,22 +18,26 @@ inline std::atomic<uint32_t> g_pitch_shifter_underruns{0};
 #endif
 
 // Stub for SectionProfiler class
-template <size_t MaxSections>
-class SectionProfiler {
+template <size_t MaxSections> class SectionProfiler {
 public:
-  explicit SectionProfiler(uint32_t /*print_interval_ms*/ = 2000) {}
-  size_t add_section(const char* /*name*/) { return 0; }
-  void record_duration(size_t /*index*/, uint64_t /*duration_us*/) {}
-  void check_and_print_report() {}
+  explicit SectionProfiler(uint32_t /*print_interval_ms*/ = 2000) {
+  }
+  size_t add_section(const char * /*name*/) {
+    return 0;
+  }
+  void record_duration(size_t /*index*/, uint64_t /*duration_us*/) {
+  }
+  void check_and_print_report() {
+  }
 };
 
 // Stub for ScopedProfile class
-template <size_t MaxSections>
-class ScopedProfile {
+template <size_t MaxSections> class ScopedProfile {
 public:
-  ScopedProfile(SectionProfiler<MaxSections>& /*profiler*/, size_t /*section_index*/) {}
-  ScopedProfile(const ScopedProfile&) = delete;
-  ScopedProfile& operator=(const ScopedProfile&) = delete;
+  ScopedProfile(SectionProfiler<MaxSections> & /*profiler*/, size_t /*section_index*/) {
+  }
+  ScopedProfile(const ScopedProfile &) = delete;
+  ScopedProfile &operator=(const ScopedProfile &) = delete;
 };
 
 // Define the number of sections for the global profiler
@@ -44,8 +48,10 @@ inline SectionProfiler<kGlobalProfilerMaxSections> g_section_profiler;
 // Stub for LoopTimer
 class LoopTimer {
 public:
-  explicit LoopTimer(uint32_t /*print_interval_ms*/ = 1000) {}
-  void record_iteration_end() {}
+  explicit LoopTimer(uint32_t /*print_interval_ms*/ = 1000) {
+  }
+  void record_iteration_end() {
+  }
 };
 
 } // namespace DebugUtils

90c1376fb8aec0298835da522ef78acc2d7e8a0e
David Menting <david@davidmenting.nl>
2025-06-26 14:35:16 +0200

    feat: Add standard file operations for rompler

---
+++ drum/applications/rompler/standard_file_ops.h
@@ -0,0 +1,66 @@
+#ifndef PRINTING_FILE_OPS_H_CVXHHJDO
+#define PRINTING_FILE_OPS_H_CVXHHJDO
+
+#include <stdio.h>
+
+#include "etl/span.h"
+#include "etl/string_view.h"
+
+struct StandardFileOps {
+  static const unsigned BlockSize = 256;
+
+  struct Handle {
+
+    Handle(const etl::string_view &path) {
+      if (file_pointer) {
+        fclose(file_pointer);
+        // TODO: Report some error. This should not happen;
+      }
+
+      printf("Writing file: '%s'\n", path.data());
+      file_pointer = fopen(path.data(), "wb");
+      if (!file_pointer) {
+        printf("ERROR: Failed opening file\n");
+      }
+    }
+
+    void close() {
+      printf("Closing file!\n");
+      if (file_pointer) {
+        fflush(file_pointer);
+        fclose(file_pointer);
+        file_pointer = nullptr;
+      } else {
+        // TODO: Error closing a non-existent handle
+      }
+      return;
+    }
+
+    // TODO: Use Chunk instead
+    size_t write(const etl::span<const uint8_t> &bytes) {
+      // printf("Writing %i bytes\n", bytes.size());
+      if (file_pointer) {
+        const auto written = fwrite(bytes.cbegin(), sizeof(uint8_t), bytes.size(), file_pointer);
+        // printf("written: %i\n", written);
+        return written;
+      } else {
+        // TODO: Error writing to a handle that should be exist.
+        return 0;
+      }
+    }
+
+  private:
+    FILE *file_pointer = nullptr;
+  };
+
+  // Handle should close upon destruction
+  // TODO: Return optional instead, if handle could not be opened.
+  Handle open(const etl::string_view &path) {
+    // TODO: Use actual path
+    // const char *path = "/tmp_sample";
+    printf("Opening new file: %s\n", path.data());
+    return Handle(path);
+  }
+};
+
+#endif /* end of include guard: PRINTING_FILE_OPS_H_CVXHHJDO */

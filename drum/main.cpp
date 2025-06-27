2378e26ef11a31ad199808a805064a63a86377ca
David Menting <david@davidmenting.nl>
2025-06-27 13:39:26 +0200

    chore: Remove unnecessary comments and update pico-extras

--- drum/main.cpp
+++ drum/main.cpp
@@ -142,19 +142,18 @@ int main() {
       if (config_manager.load()) {
         sample_repository.load_from_config(config_manager.get_sample_configs());
       }
-      new_file_received = false; // Reset the flag
+      new_file_received = false;
     }
 
     pizza_controls.update();
     audio_engine.process();
 
-    // Update the display
     pizza_display.update(get_absolute_time());
 
     musin::usb::background_update();
-    midi_read();                              // TODO: turn this into a musin input queue
-    tempo_handler.update();                   // Call TempoHandler update for auto-switching logic
-    musin::midi::process_midi_output_queue(); // Process the outgoing MIDI queue
+    midi_read(); // TODO: turn this into a musin input queue
+    tempo_handler.update();
+    musin::midi::process_midi_output_queue();
 
     loop_timer.record_iteration_end();
   }

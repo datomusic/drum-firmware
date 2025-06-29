e5ca768d3f9b057397d5e6c21df17783b18a45b1
David Menting <david@davidmenting.nl>
2025-06-29 17:26:39 +0200

    fix: Reduce SysEx chunk size and update build includes

--- musin/musin_init.cmake
+++ musin/musin_init.cmake
@@ -114,7 +114,8 @@ macro(musin_setup_filesystem_target)
     # Implementation needs include paths to find musin headers
     target_include_directories(musin_filesystem_impl PRIVATE
         ${MUSIN_ROOT}/..
-        ${MUSIN_ROOT}/ports/pico/libraries/pico-vfs/littlefs
+        ${MUSIN_ROOT}/ports/pico/libraries/pico-vfs/vendor/littlefs
+        ${MUSIN_ROOT}/ports/pico/pico-sdk/src/rp2_common/hardware_flash/include
     )
 
     # Implementation needs pico stdlib and the vfs library to compile

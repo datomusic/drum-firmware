8bef2336ce3e4166474e81537dfece84470a0d77
David Menting <david@davidmenting.nl>
2025-07-02 13:58:45 +0200

    chore: remove no longer needed hard-coded samples

--- drum/sb25_samples.h
+++
@@ -1,9 +0,0 @@
-#ifndef DRUM_SB25_SAMPLES_H_
-#define DRUM_SB25_SAMPLES_H_
-
-#include "etl/array.h"
-#include "musin/audio/sample_data.h"
-
-extern const etl::array<musin::SampleData, 32> all_samples;
-
-#endif // DRUM_SB25_SAMPLES_H_

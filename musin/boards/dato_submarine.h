745317f5f6cbe0faf15893140b0d1b2582e133bf
David <david@davidmenting.nl>
2025-07-11 17:55:48 +0200

    fix: make board definition override fs size (#344)

--- musin/boards/dato_submarine.h
+++ musin/boards/dato_submarine.h
@@ -53,9 +53,7 @@
 #ifndef PICO_FLASH_SPI_CLKDIV
 #define PICO_FLASH_SPI_CLKDIV 2
 #endif
-#ifndef PICO_FS_DEFAULT_SIZE
 #define PICO_FS_DEFAULT_SIZE (4 * 1024 * 1024)
-#endif
 #ifndef PICO_FLASH_SIZE_BYTES
 #define PICO_FLASH_SIZE_BYTES (8 * 1024 * 1024)
 #endif

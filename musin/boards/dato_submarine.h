8f1d9ef6b5abdd5ac9ff87cb8a6ddec4e322f1ff
David Menting <david@davidmenting.nl>
2025-07-31 10:14:46 +0200

    feat: add DIN MIDI pin definitions

--- musin/boards/dato_submarine.h
+++ musin/boards/dato_submarine.h
@@ -21,6 +21,14 @@
 #define PICO_DEFAULT_UART_RX_PIN 1
 #endif
 
+// --- MIDI UART ---
+#ifndef DATO_SUBMARINE_MIDI_TX_PIN
+#define DATO_SUBMARINE_MIDI_TX_PIN 4
+#endif
+#ifndef DATO_SUBMARINE_MIDI_RX_PIN
+#define DATO_SUBMARINE_MIDI_RX_PIN 5
+#endif
+
 // --- I2C ---
 #ifndef PICO_DEFAULT_I2C
 #define PICO_DEFAULT_I2C 0

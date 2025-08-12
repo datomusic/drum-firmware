1894b05fff95ab3a7e5a0ed01308025bfa0d0760
David Menting <david@davidmenting.nl>
2025-08-12 13:04:14 +0200

    fix: Remove const from SysExTransferStateChangeEvent param

--- drum/main.cpp
+++ drum/main.cpp
@@ -56,7 +56,7 @@ static ApplicationState current_state = ApplicationState::SequencerMode;
 struct StateMachineObserver
     : public etl::observer<drum::Events::SysExTransferStateChangeEvent> {
   void notification(
-      const drum::Events::SysExTransferStateChangeEvent &event) override {
+      drum::Events::SysExTransferStateChangeEvent &event) {
     if (event.is_active) {
       logger.debug("Entering FileTransferMode");
       current_state = ApplicationState::FileTransferMode;
